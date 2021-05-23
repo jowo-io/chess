76d15aa64ad6b42d962de98e39bc9b62ad2f2554
only-hosting <only.hosting123@gmail.com>
2021-05-23 15:05:31 +0100

    move images, improved code

--- app/src/App.js
+++ app/src/App.js
@@ -1,55 +1,56 @@
 import './App.css';
+import blackRook from './assets/blackRook.png'
+
+const pieceTypes = {
+  BLACK_ROOK: 1,
+};
+
+function renderPiece(piece) {
+  switch(piece){
+    case pieceTypes.BLACK_ROOK:
+      return (
+        <img src={blackRook} alt="Black Rook" />
+      );
+  }
+}
+
+function iteratePieces(piece) {
+  return (
+    <div>
+      {renderPiece(piece)}
+    </div>
+  );
+}
+
+function iterateRows(row) {
+  return (
+    <div>
+      {row.map(iteratePieces)}
+    </div>
+  );
+}
+
+function iterateBoard(board) {
+  return board.map(iterateRows)
+}
+
+let board = [
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 1, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 1, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+];
 
 function App() {
-  function iterateRows(row) {
-    row.forEach(iteratePieces)
-  }
-
-  function iteratePieces(piece) {
-    console.log(piece)
-  }
-
-  function iterateBoard(board) {
-    board.forEach(iterateRows)
-  }
-
-  let board = [
-    [0, 1, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 1, 0, 0]
-  ];
-
-  iterateBoard(board);
-  
   return (
     <div className="App">
       <h1>Chess Board!</h1>
       <div class="chessboard">
-        <div>
-            <div></div><div></div><div></div><div></div><div><img src="/images/blackRook.png" alt=""/></div><div></div><div></div><div></div>
-        </div>
-        <div>
-            <div></div><div><img src="/images/blackQueen.png" alt=""/></div><div></div><div></div><div></div><div></div><div></div><div></div>
-        </div>
-        <div>
-            <div></div><div></div><div></div><div></div><div></div><div></div><div><img src="/images/whitePawn.png" alt=""/></div><div></div>
-        </div>
-        <div>
-            <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
-        </div>
-        <div>
-            <div></div><div></div><div><img src="/images/whiteBishop.png" alt=""/></div><div></div><div></div><div></div><div></div><div></div>
-        </div>
-        <div>
-            <div></div><div></div><div><img src="/images/whiteKing.png" alt=""/></div><div></div><div></div><div><img src="/images/blackKnight.png" alt=""/></div><div></div><div></div>
-        </div>
-        <div>
-            <div><img src="/images/whiteRook.png" alt=""/></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
-        </div>
-        <div>
-            <div></div><div></div><div></div><div><img src="/images/blackKing.png" alt=""/></div><div></div><div></div><div></div><div></div>
-        </div>
+        {iterateBoard(board)}
       </div>
     </div>
   );
